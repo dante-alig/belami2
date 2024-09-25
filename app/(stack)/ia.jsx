a4d586fd63be8b251a91ed57cb8a192e473125a7
dante-alig <samuel.celeste@gmail.com>
2024-09-25 09:44:08 -0400

    style

--- app/(stack)/ia.jsx
+++ app/(stack)/ia.jsx
@@ -1,19 +1,37 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Pressable, Image } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import visuel from "../../images/visuel.png";
+import Constants from "expo-constants";
+
 export default function IaScreen() {
   return (
     <>
       <StatusBar translucent={true} style="light" />
       <View style={styles.container}>
-        <Text style={styles.title}>Bienvenue sur Expo Router !</Text>
+        <View>
+          <Text style={styles.title}>Belami</Text>
+        </View>
+        <Image source={visuel} style={styles.image} />
+        <View style={styles.buttonBox}>
+          <Pressable style={styles.button2}>
+            <Text style={styles.titlePres}>Assistant IA</Text>
+            <Text style={styles.txtPres}>
+              Recevez le message parfait à lui envoyer pour augmenter vos
+              chances d’obtenir un rendez-vous avec elle.{" "}
+            </Text>
+          </Pressable>
+          <Pressable style={styles.button}>
+            <Text style={styles.txtCenter}>Commencer</Text>
+          </Pressable>
+        </View>
       </View>
     </>
   );
 }
-
+const marginStatus = 30 + Constants.statusBarHeight;
 const styles = StyleSheet.create({
   container: {
-    justifyContent: "center",
+    justifyContent: "space-between",
     alignItems: "center",
     backgroundColor: "black",
     height: "100%",
@@ -23,5 +41,40 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "bold",
     color: "#ffffff",
+    marginTop: marginStatus,
+  },
+  buttonBox: {
+    width: "100%",
+  },
+  titlePres: {
+    color: "white",
+    fontSize: 23,
+    textTransform: "uppercase",
+  },
+  txtPres: {
+    color: "white",
+  },
+
+  button2: {
+    padding: 20,
+    width: "100%",
+    marginBottom: 10,
+    borderRadius: 30,
+    backgroundColor: "#101010",
+  },
+  button: {
+    backgroundColor: "#D9CDA0",
+    height: 56,
+    marginBottom: 10,
+    borderRadius: 30,
+    justifyContent: "center",
+    width: "100%",
+  },
+  txtCenter: {
+    textAlign: "center",
+  },
+  image: {
+    height: 451,
+    width: "100%",
   },
 });
--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { Tabs } from "expo-router";
 import { View, StyleSheet } from "react-native";
+import ExpertUser from "../icon/User_expert.svg";
+import ProfilUser from "../icon/User_Circle.svg";
+import Tchat from "../icon/Chat.svg";
+import ExpertUserActive from "../icon/User_expert_active.svg";
+import ProfilUserActive from "../icon/User_Circle_active.svg";
+import TchatActive from "../icon/Chat_active.svg";
 
 export default function Layout() {
   return (
@@ -10,19 +16,54 @@ export default function Layout() {
           headerShown: false,
           tabBarStyle: {
             backgroundColor: "#141414", // Couleur de fond
-            height: 70, // Change la hauteur du menu tabs
-            borderRadius: 30, // Applique un border-radius
-            marginBottom: 40, // Ajoute un espacement en bas pour mieux voir le border-radius
-            paddingBottom: 10, // Ajuste l'intérieur de la barre pour les icônes
+            height: 75, // Change la hauteur du menu tabs
+            borderRadius: 0, // Applique un border-radius
+            marginBottom: 0, // Ajoute un espacement en bas pour mieux voir le border-radius
+            paddingBottom: 20, // Ajuste l'intérieur de la barre pour les icônes
             paddingTop: 10, // Ajuste l'intérieur de la barre pour les icônes
+            borderBlockColor: "black",
           },
-          tabBarActiveTintColor: "#B0AFAB", // Couleur du texte actif
-          tabBarInactiveTintColor: "#2E2E2E", // Couleur du texte inactif
+          tabBarActiveTintColor: "#B0AFAB", // Couleur des icônes actifs
+          tabBarInactiveTintColor: "#2E2E2E", // Couleur des icônes inactifs
+          // tabBarLabelStyle: { display: "none" }, // Masque les labels (titres)
         }}
       >
-        <Tabs.Screen name="expert" />
-        <Tabs.Screen name="(stack)/ia" />
-        <Tabs.Screen name="profil" />
+        <Tabs.Screen
+          name="expert"
+          options={{
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <ExpertUserActive width={29} height={29} />
+              ) : (
+                <ExpertUser width={29} height={29} />
+              ),
+            tabBarLabel: "Expert", // Aucune étiquette (texte)
+          }}
+        />
+        <Tabs.Screen
+          name="(stack)/ia"
+          options={{
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <TchatActive width={29} height={29} />
+              ) : (
+                <Tchat width={29} height={29} />
+              ),
+            tabBarLabel: "IA", // Aucune étiquette (texte)
+          }}
+        />
+        <Tabs.Screen
+          name="profil"
+          options={{
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <ProfilUserActive width={29} height={29} />
+              ) : (
+                <ProfilUser width={29} height={29} />
+              ),
+            tabBarLabel: "Profil", // Aucune étiquette (texte)
+          }}
+        />
         <Tabs.Screen
           name="index"
           options={{
